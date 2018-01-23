923f5ef1ab02d0460c868bef4148017dc14e70ec
MatteoCampinoti94 <matteo.campinoti94@gmail.com>
2018-01-23 02:44:54 +0100

    Fixed variable name error

--- md2html.py
+++ md2html.py
@@ -23,19 +23,19 @@ Q = 0
 p = False
 i = 0
 
-for c in ifile.read():
+for ch in ifile.read():
     i += 1
 
-    if c != '\n' and not p:
+    if ch != '\n' and not p:
         ofile.write('<p>')
         p = True
 
-    if c in ('*', '_'):
+    if ch in ('*', '_'):
         if C:
-            ofile.write(c)
+            ofile.write(ch)
             continue
-        c = ifile.read()
-        if c in ('*', '_'):
+        ch = ifile.read()
+        if ch in ('*', '_'):
             ofile.write(f'<{"/"*B}b>')
             B = not B
         else:
@@ -43,36 +43,36 @@ for c in ifile.read():
             ofile.write(f'<{"/"*I}i>')
             I = not I
 
-    elif c == '`':
-        c = ifile.read()
-        if c == '`':
-            c = ifile.read()
-            if c == '`':
+    elif ch == '`':
+        ch = ifile.read()
+        if ch == '`':
+            ch = ifile.read()
+            if ch == '`':
                 ofile.write(f'<{"/"*C}code>')
                 C = not C
             else:
                 if C:
-                    ofile.write('``'+c)
+                    ofile.write('``'+ch)
                     continue
                 i -= 1
                 ofile.write(f'<code></code>')
         else:
             if C:
-                ofile.write('`'+c)
+                ofile.write('`'+ch)
                 continue
             i -= 1
             ofile.write(f'<{"/"*c}code>')
             c = not c
 
-    elif c == '>':
+    elif ch == '>':
         ofile.write('<blockquote>')
         Q += 1
 
-    elif c == '\n':
+    elif ch == '\n':
         if not p:
             ofile.write('<br>\n')
-        c = ifile.read()
-        if c == '\n':
+        ch = ifile.read()
+        if ch == '\n':
             ofile.write('</blockquote>'*Q)
             ofile.write('</p>\n\n')
             p == False
@@ -82,6 +82,6 @@ for c in ifile.read():
             i -= 1
 
     else:
-        ofile.write(c)
+        ofile.write(ch)
 
     ifile.seek(i)
